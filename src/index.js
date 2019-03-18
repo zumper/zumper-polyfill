dd8fd36151ba01e0faee76ebb848dd6bd6c73d6d
Justin Jeong <justin.jeong@zumper.com>
2019-03-18 16:19:25 -0700

    Polyfill update for scroll behavior

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import '@babel/polyfill'
 
 import './intersectionObserver'
 import './requestIdleCallback'
+import './smoothScroll'
 
 // NOTE: Conditional require calls can't exist in the same file as import statements in rollup.
 // https://github.com/rollup/rollup/issues/1058#issuecomment-254187433
---
+++ src/smoothScroll.js
@@ -0,0 +1,12 @@
+// Check for scroll behavior (safari), and scroll methods (ie11 and others)
+require('smoothscroll-polyfill')
+const w = window;
+const d = document;
+const E = w.HTMLElement || w.Element
+if (
+  !('scrollBehavior' in d.documentElement.style) ||
+  (!w.scroll || !w.scrollTo || !w.scrollBy) ||
+  !E.prototype.scrollIntoView
+) {
+  smoothscroll.polyfill()
+}
