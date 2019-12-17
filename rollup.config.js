94f3c3eddc458d7f97b6ca927ced5505ac10881f
Grady Kuhnline <github@heygrady.net>
2019-12-17 11:20:32 -0800

    upgrade packages to address security warning

--- rollup.config.js
+++ rollup.config.js
@@ -5,7 +5,7 @@
  */
 
 import commonjs from 'rollup-plugin-commonjs'
-import nodeResolve from 'rollup-plugin-node-resolve'
+import nodeResolve from '@rollup/plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import replace from '@rollup/plugin-replace'
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
