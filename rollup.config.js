2c53585954f67d41d9ab0b27b8f18eb32eb97e4e
Grady Kuhnline <heygrady@gmail.com>
2021-07-13 10:58:18 -0700

    chore: upgrade deps

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,9 @@
  * @description  rollup.config.js
  */
 
+import babel from '@rollup/plugin-babel'
 import commonjs from '@rollup/plugin-commonjs'
 import nodeResolve from '@rollup/plugin-node-resolve'
-import babel from 'rollup-plugin-babel'
 import replace from '@rollup/plugin-replace'
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
 import { terser } from 'rollup-plugin-terser'
